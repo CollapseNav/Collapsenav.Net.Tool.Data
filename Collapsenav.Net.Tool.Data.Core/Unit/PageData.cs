d1586aac03dbbb085dcf1d7866fae85d02fb2c64
CollapseNav <CollapseNav@163.com>
2023-06-17 23:50:08 +0800

    feat: 添加分页用的pagedata和pagerequest

---
+++ Collapsenav.Net.Tool.Data.Core/Unit/PageData.cs
@@ -0,0 +1,20 @@
+namespace Collapsenav.Net.Tool.Data;
+public class PageData : PageData<object>
+{
+}
+public class PageData<T>
+{
+    public int? Total { get; set; }
+    public int? Length { get => Data?.Count(); }
+    public IEnumerable<T> Data { get => _data; set => _data = value; }
+    private IEnumerable<T> _data;
+    public PageData() { }
+    public PageData(int? total, IEnumerable<T> data) : this(data, total)
+    {
+    }
+    public PageData(IEnumerable<T> data, int? total = null)
+    {
+        Total = total ?? data?.Count();
+        Data = data;
+    }
+}
