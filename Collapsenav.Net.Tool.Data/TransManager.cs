e169bf45367c56d8f7cabce0cc6ee74b7e3b9326
CollapseNav <CollapseNav@163.com>
2023-07-26 02:11:29 +0800

    feat: 完善TransManager

--- Collapsenav.Net.Tool.Data/TransManager.cs
+++ Collapsenav.Net.Tool.Data/TransManager.cs
@@ -5,16 +5,30 @@ namespace Collapsenav.Net.Tool.Data;
 public class TransManager
 {
     private static Dictionary<DbContext, long> ContextCount { get; set; } = new();
+    public static bool AutoCommit = false;
+    public static bool HasError = false;
+    public static void UseAutoCommit()
+    {
+        AutoCommit = true;
+    }
+    public static void HasException()
+    {
+        HasError = true;
+    }
     public static void Add(DbContext context)
     {
+        if (!AutoCommit)
+            return;
+        HasError = false;
         if (ContextCount.ContainsKey(context))
             ContextCount[context]++;
         else
             ContextCount.Add(context, 1);
     }
-
     public static void Remove(DbContext context)
     {
+        if (!HasError)
+            return;
         if (ContextCount.ContainsKey(context))
         {
             ContextCount[context]--;
