4f2c8c3e0831b0cb53a6d7a136c87a4cf3b001ad
CollapseNav <jwpwdwd@163.com>
2024-02-05 09:32:42 +0800

    demo: 尝试自动提交

--- Demo/AspnetDbDemo/Program.cs
+++ Demo/AspnetDbDemo/Program.cs
@@ -1,4 +1,3 @@
-using AspnetDbDemo.Controllers;
 using Collapsenav.Net.Tool.Data;
 using Collapsenav.Net.Tool.WebApi;
 using DataDemo.EntityLib;
@@ -6,12 +5,13 @@ using DataDemo.EntityLib;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddMariaDb<EntityContext>(new MariaDbConn("localhost", 3306, "DataDemo", "root", "abc@123"));
+builder.Services.AddMariaDb<EntityContext>(new MariaDbConn("localhost", 3306, "DataDemo", "root", "asd@123"));
 // builder.Services.AddSqlitePool<EntityContext>("./Data.db");
 builder.Services.AddDefaultDbContext<EntityContext>();
 builder.Services.AddDefaultSwaggerGen();
 builder.Services.AddRepository();
 var app = builder.Build();
+app.UseAutoCommit();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
